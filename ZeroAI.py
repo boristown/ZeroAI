b33bad55a1d4a70e3cfb34b32aee5484dcc9c183
boristown <boristown@gmail.com>
2019-04-18 17:03:39 +0800

    迭代3：输出股市HTML文件清单

--- ZeroAI.py
+++ ZeroAI.py
@@ -1,3 +1,13 @@
+import os
+import glob
 import re
 
+html_path = "HTML/"
+Equities_path = os.path.join(html_path, "*股市*.htm*")
+Equities_pattern = r'<td\sclass="bold\sleft\snoWrap\selp\splusIconTd">.+?boundblank="">([^><"]+)</a><span.+?data-id="(\d+)"'
+dirs = glob.glob( Equities_path )
 
+url = "https://www.investing.com/instruments/HistoricalDataAjax"
+
+for file_path in dirs:
+    print( file_path )

a91cdb44e1f0b427e647ec8e2aa9ed987ef1bf71
boristown <boristown@gmail.com>
2019-04-18 17:31:07 +0800

    迭代5：输出两市清单到txt

--- ZeroAI.py
+++ ZeroAI.py
@@ -15,6 +15,9 @@ for file_path in dirs:
     file_str = file.read()
     symbol_index = 0
     symbols_match = re.finditer(Equities_pattern,file_str,re.S)
+    list_filename = os.path.join('Output/Stocks/' + os.path.basename(file_path).split('.')[0] + '.txt')
+    list_file = open(list_filename, "w")
+    list_file.truncate()
     #end_date_str = (datetime.datetime.utcnow() + datetime.timedelta(days = -1)).strftime("%m-%d-%Y").replace("-","%2F")
     for symbol_match in symbols_match:
         symbol_index+=1
@@ -22,4 +25,6 @@ for file_path in dirs:
         if int(curr_id_str) < 1:
            continue
         symbol_str = symbol_match.group(1)
+        list_file.write(symbol_str+'\n')
         print( "%d\t%s\t%s" % (symbol_index, curr_id_str, symbol_str)  )
+    list_file.close()
